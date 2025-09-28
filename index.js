a342529a726fb1f1212ccd0bc8fe7dae41ef44d9
Monjur Elahi Touheed <monjurelahitouheed@gmail.com>
2025-09-28 22:14:43 +0600

    404 page added

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
 const route = require('./routes');
 const dbConnection = require('./database/dbConnection');
+dbConnection()
 
 app.use(express.json());
 app.use(route);
 
-dbConnection()
+app.use(express.static(path.join(__dirname, "view")));
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, "view", "404.html"))
+})
 
 app.listen(port, () => {
     console.log('This app is listening on port', port);
